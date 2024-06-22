19ead430a5f7d681e34ad5aeff8251fddb239206
Nathan Larose <nathanlarose07@gmail.com>
2024-06-21 20:49:29 -0400

    first commit

---
+++ js-header-footer/integration.js
@@ -0,0 +1,93 @@
+// ------------------------------------------------------------------------------------------------------------ //
+
+
+//footer_ANGLAIS
+var footer = new XMLHttpRequest();
+footer.open("GET", "js-header-footer/footer.html", true);
+
+footer.onreadystatechange = function () {
+    if (footer.readyState === 4 && footer.status === 200) {
+        document.getElementById("footer").innerHTML = footer.responseText;
+    }
+};
+
+// Function to send the request after a delay
+function loadFooterWithDelay() {
+    footer.send();
+}
+
+// Set a delay of 200 milliseconds before sending the request
+setTimeout(loadFooterWithDelay, 1000);
+
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+
+//header_ANGLAIS
+var navbar = new XMLHttpRequest();
+navbar.open("GET", "js-header-footer/header.html", true);
+
+navbar.onreadystatechange = function () {
+    if (navbar.readyState === 4 && navbar.status === 200) {
+        document.getElementById("header").innerHTML = navbar.responseText;
+    }
+};
+
+// Function to send the request after a delay
+function loadHeaderWithDelay() {
+    navbar.send();
+}
+
+// Set a delay of 200 milliseconds before sending the request
+setTimeout(loadHeaderWithDelay, 1000);
+
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+
+//header_FRANCAIS
+var navbar_fr = new XMLHttpRequest();
+navbar_fr.open("GET", "js-header-footer/header_french.html", true);
+
+navbar_fr.onreadystatechange = function () {
+    if (navbar_fr.readyState === 4 && navbar_fr.status === 200) {
+        document.getElementById("header_french").innerHTML = navbar_fr.responseText;
+    }
+};
+
+// Function to send the request after a delay
+function loadFrenchHeaderWithDelay() {
+    navbar_fr.send();
+}
+
+// Set a delay of 200 milliseconds before sending the request
+setTimeout(loadFrenchHeaderWithDelay, 1000);
+
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+
+//footer_FRANCAIS
+var footer_french = new XMLHttpRequest();
+footer_french.open("GET", "js-header-footer/footer_french.html", true);
+
+footer_french.onreadystatechange = function () {
+    if (footer_french.readyState === 4 && footer_french.status === 200) {
+        document.getElementById("footer_french").innerHTML = footer_french.responseText;
+    }
+};
+
+// Function to send the request after a delay
+function loadFrenchFooterWithDelay() {
+    footer_french.send();
+}
+
+// Set a delay of 200 milliseconds before sending the request
+setTimeout(loadFrenchFooterWithDelay, 1000);
+
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+
+
+ 
